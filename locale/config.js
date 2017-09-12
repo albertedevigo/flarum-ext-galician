e466b56ab9d031d229b2dd9bb53291369212d486
alberte <albertegz@gmail.com>
2017-09-12 09:27:15 +0200

    Updated moment translation

--- locale/config.js
+++ locale/config.js
@@ -1,44 +1,77 @@
-moment.locale('gl', {
-	months : 'Xaneiro_Febreiro_Marzo_Abril_Maio_Xuño_Xullo_Agosto_Setembro_Outubro_Novembro_Decembro'.split('_'),
-	monthsShort : 'Xan_Feb_Mar_Abr_Mai_Xun_Xul_Ago_Set_Out_Nov_Dec'.split('_'),
-	weekdays : 'Domingo_Segunda-Feira_Terza-Feira_Cuarta-Feira_Quinta-Feira_Sexta-Feira_Sábado'.split('_'),
-	weekdaysShort : 'Dom_Seg_Ter_Cua_Qui_Sex_Sáb'.split('_'),
-	weekdaysMin : 'Dom_2ª_3ª_4ª_5ª_6ª_Sáb'.split('_'),
-	longDateFormat : {
-		LT : 'HH:mm',
-		LTS : 'HH:mm:ss',
-		L : 'DD/MM/YYYY',
-		LL : 'D [de] MMMM [de] YYYY',
-		LLL : 'D [de] MMMM [de] YYYY [ás] HH:mm',
-		LLLL : 'dddd, D [de] MMMM [de] YYYY [ás] HH:mm'
-	},
-	calendar : {
-		sameDay: '[Hoxe ás] LT',
-		nextDay: '[Mañá ás] LT',
-		nextWeek: 'dddd [ás] LT',
-		lastDay: '[Onte ás] LT',
-		lastWeek: function () {
-		return (this.day() === 0 || this.day() === 6) ?
-		'[Último] dddd [ás] LT' : // Saturday + Sunday
-		'[Última] dddd [ás] LT'; // Monday - Friday
-	},
-	sameElse: 'L'
-	},
-	relativeTime : {
-		future : 'en %s',
-		past : 'fai %s',
-		s : 'poucos segundos',
-		m : 'un minuto',
-		mm : '%d minutos',
-		h : 'unha hora',
-		hh : '%d horas',
-		d : 'un día',
-		dd : '%d días',
-		M : 'un mes',
-		MM : '%d meses',
-		y : 'un ano',
-		yy : '%d anos'
-	},
-	ordinalParse: /\d{1,2}º/,
-	ordinal : '%dº'
+//! moment.js locale configuration
+//! locale : Galician [gl]
+//! author : https://github.com/moment/moment/blob/develop/locale/gl.js
+
+;(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined'
+       && typeof require === 'function' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['../moment'], factory) :
+   factory(global.moment)
+}(this, (function (moment) { 'use strict';
+
+
+var gl = moment.defineLocale('gl', {
+    months : 'xaneiro_febreiro_marzo_abril_maio_xuño_xullo_agosto_setembro_outubro_novembro_decembro'.split('_'),
+    monthsShort : 'xan._feb._mar._abr._mai._xuñ._xul._ago._set._out._nov._dec.'.split('_'),
+    monthsParseExact: true,
+    weekdays : 'domingo_segunda-feira_terza-feira_cuarta-feira_quinta-feira_sexta-feira_sábado'.split('_'),
+    weekdaysShort : 'dom_seg_ter_cua_qui_sex_sáb'.split('_'),
+    weekdaysMin : 'dom_2ª_3ª_4ª_5ª_6ª_sáb'.split('_'),
+    weekdaysParseExact : true,
+    longDateFormat : {
+        LT : 'H:mm',
+        LTS : 'H:mm:ss',
+        L : 'DD/MM/YYYY',
+        LL : 'D [de] MMMM [de] YYYY',
+        LLL : 'D [de] MMMM [de] YYYY H:mm',
+        LLLL : 'dddd, D [de] MMMM [de] YYYY H:mm'
+    },
+    calendar : {
+        sameDay : function () {
+            return '[hoxe ' + ((this.hours() !== 1) ? 'ás' : 'á') + '] LT';
+        },
+        nextDay : function () {
+            return '[mañá ' + ((this.hours() !== 1) ? 'ás' : 'á') + '] LT';
+        },
+        nextWeek : function () {
+            return 'dddd [' + ((this.hours() !== 1) ? 'ás' : 'a') + '] LT';
+        },
+        lastDay : function () {
+            return '[onte ' + ((this.hours() !== 1) ? 'á' : 'a') + '] LT';
+        },
+        lastWeek : function () {
+            return '[o] dddd [pasado ' + ((this.hours() !== 1) ? 'ás' : 'a') + '] LT';
+        },
+        sameElse : 'L'
+    },
+    relativeTime : {
+        future : function (str) {
+            if (str.indexOf('un') === 0) {
+                return 'n' + str;
+            }
+            return 'en ' + str;
+        },
+        past : 'hai %s',
+        s : 'uns segundos',
+        m : 'un minuto',
+        mm : '%d minutos',
+        h : 'unha hora',
+        hh : '%d horas',
+        d : 'un día',
+        dd : '%d días',
+        M : 'un mes',
+        MM : '%d meses',
+        y : 'un ano',
+        yy : '%d anos'
+    },
+    dayOfMonthOrdinalParse : /\d{1,2}º/,
+    ordinal : '%dº',
+    week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 4  // The week that contains Jan 4th is the first week of the year.
+    }
 });
+
+return gl;
+
+})));
